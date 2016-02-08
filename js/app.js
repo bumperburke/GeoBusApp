d38625b54b4160c37d0874bf82b0c97c51e7762a
Stefan Burke <stefan.burke@mydit.ie>
2016-02-08 21:08:42 +0000

    Login and Register done for now. Small UI changes after Server-side work.

--- js/app.js
+++ js/app.js
@@ -41,61 +41,49 @@ app.config(function($stateProvider, $urlRouterProvider) {
         controller: ''
     });
 });
-/*app.config(function ($routeProvider) {
-    $routeProvider
-
-        .when('/', {
-        templateUrl: 'html/greeting.html',
-        controller: ''
-    })
-
-    .when('/timetables', {
-        templateUrl: 'html/timetables.html',
-        controller: ''
-    })
-
-    .when('/login', {
-        templateUrl: 'html/loginContent.html',
-        controller: 'loginCtrl'
-    })
-
-    .when('/createAccount', {
-        templateUrl: 'html/newAccount.html',
-        controller: 'newAccountCtrl'
-    })
-
-    .when('/home', {
-        templateUrl: 'html/home.html',
-        controller: 'homeCtrl'
-    });
-
-});*/
 
 app.run(function (defaultErrorMessageResolver) {
     defaultErrorMessageResolver.getErrorMessages().then(function (errorMessages) {
-        errorMessages['passFail'] = 'Must contain upper and lower case letters and numbers';
-        errorMessages['mismatch'] = 'Passwords Do Not Match!';
+        errorMessages['nameError'] = 'Only letters to be used! (A-Z, a-z)';
+        errorMessages['passError'] = 'Upper and lower case letters and numbers only!';
+        errorMessages['mismatchPassError'] = 'Passwords Do Not Match!';
     })
 })
 
-app.controller('loginCtrl', function ($scope, $http, $location) {
+app.controller('loginCtrl', function ($scope, $http) {
     $scope.btnText = "Login";
-    /*$scope.users = [];*/
+    $scope.loginCreds = {};
+    $scope.showResponse = false;
+    $scope.logging = false;
 
-    $scope.login = function () {
+    $scope.submitLogin = function () {
         $scope.loggingIn = true;
         $scope.btnText = "Processing...";
 
+        var request = $http({
+            method: 'post',
+            url: 'https://geobus.co.uk/api/v1/login',
+            data: $scope.loginCreds,
+            headers: {'Content-Type' : 'application/x-www-form-urlencoded; charset=utf-8'}
+        })
+        .then(function(response){
+            if(response.data.message === 'credFail'){
+                $scope.showResponse = true;
+                $scope.logging = false;
+                $scope.loginResponse = 'Invalid Login Credentials!';
+                $scope.respLoginMsgColor = {"color":"#ff0000"};
+                $scope.btnText = "Login";
+            }
+            else if(response.data.message === 'confirmed') {
+                $scope.showResponse = true;
+                $scope.logging = false;
+                $scope.loginResponse = 'Success';
+                $scope.respLoginMsgColor = {"color":"#228b22"};
+            }
+        });
     }
 
-    /*$scope.getTest = function() {
-        $http.get("https://geobus.co.uk/api/getUsers.php")
-        .then(function(response){
-            $scope.users = response.data;
-        }, function(response) {
-            $scope.users = "ERROR";
-        });
-    }*/
+    
 });
 
 app.controller('homeCtrl', function ($scope) {
@@ -105,23 +93,48 @@ app.controller('homeCtrl', function ($scope) {
 app.controller('newAccountCtrl', ['$scope', '$http', function ($scope, $http) {
     $scope.formData = {};
     $scope.createBtn = "Create Account";
+    $scope.showForm = true;
 
 
     $scope.formSubmit = function () {
         $scope.registering = true;
         $scope.createBtn = "Registering Details...";
+        $scope.responseMsgColor = {};
 
         var request = $http({
                 method: 'post',
-                url: 'https://geobus.co.uk/api/addUser/add.php',
+                url: 'https://geobus.co.uk/api/v1/register',
                 data: $scope.formData,
-                headers: { 'Content-Type' : 'application/json' }
+                headers: {'Content-Type' : 'application/x-www-form-urlencoded; charset=utf-8'}
+            })
+            .then(function(response) {
+                $scope.registering = false;
+                $scope.showResponse = false;
+                $scope.disableButton = false;
+                
+                if(response.data.message === 'success')
+                {
+                    $scope.createBtn = "Success!";
+                    $scope.postResponse = "Registration Successful!";
+                    $scope.showResponse = true;
+                    $scope.responseMsgColor = {"color":"#228b22"};
+                    $scope.disableButton = true;
+                    $scope.showForm = false;
+                }
+                else if(response.data.message === 'fail'){
+                    $scope.createBtn = "Oooops!";
+                    $scope.postResponse = "Sorry, An Error Has Occured! Re-try Later.";
+                    $scope.showResponse = true;
+                    $scope.responseMsgColor = {"color":"#ff0000"};
+                    $scope.disableButton = true;
+                    $scope.showForm = true;
+                }
+                else if(response.data.message === 'duplicate'){
+                    $scope.createBtn = "Oooops!";
+                    $scope.postResponse = "Sorry, That Email Is Already In Use! Please Use Another.";
+                    $scope.showResponse = true;
+                    $scope.responseMsgColor = {"color":"#ff0000"};
+                }
             });
-        
-        request.success(function(data) {
-            $scope.registering = false;
-            $scope.createBtn = "Success!";
-            $scope.postResponse = data.reqSuccess;
-        });
     }
 }]);
