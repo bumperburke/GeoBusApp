7abf4f89b7fcbaef94bd801e397292e2b45500dc
Stefan Burke <stefan.burke@mydit.ie>
2016-03-18 14:35:59 +0000

    Changed Resources to Local. Map features added.

--- js/getTimetable.js
+++ js/getTimetable.js
@@ -1,107 +1,113 @@
 $('#tableSelectError').hide();
 
 $("#selectTimetable").click(function(){
-    sessionStorage.selectedTimetable = $("#routeSelect").val();
-      
-    $.ajax({
-        type: "GET",
-        url: "https://www.geobus.co.uk/api/v1/getTimetable/"+sessionStorage.selectedTimetable,
-        success: function (data) {
-            if (data.error === false) {
-                var monFriResults = data.data.timetable.monFri;
-                var satResults = data.data.timetable.sat;
-                var sunResults = data.data.timetable.sun;
-                var message = [];
+    if($("#routeSelect").val() === "Choose a Route"){
+        $('#tableSelectError').html("<center>Please Select a Route!</center>");
+        hideShowAlert('#tableSelectError');
+        return false;
+    }else{
+        sessionStorage.selectedTimetable = $("#routeSelect").val();
+        $.ajax({
+            type: "GET",
+            url: "https://www.geobus.co.uk/api/v1/getTimetable/"+sessionStorage.selectedTimetable,
+            success: function (data) {
+                if (data.error === false) {
+                    var monFriResults = data.data.timetable.monFri;
+                    var satResults = data.data.timetable.sat;
+                    var sunResults = data.data.timetable.sun;
+                    var message = [];
 
-                var firstInStop = null;
-                var firstOutStop = null;
-                var inboundStops = [];
-                var outboundStops = [];
-                var count = 0;
-                var inNumCols = new Object();
-                var outNumCols = new Object();
-                var inbound = new Object();
-                var outbound = new Object();
-                var firstDirection = monFriResults[0].direction;
-                
-                if(monFriResults !== null){
-                    var res1 = inboundOutbound(monFriResults, firstDirection);
-                    inbound["monFri"] = res1["inbound"];
-                    outbound["monFri"] = res1["outbound"];
-                }else{message.push("No Timetable For Midweek");}
-                
-                if(satResults !== null){
-                    var res2 = inboundOutbound(satResults, firstDirection);
-                    inbound["sat"] = res2["inbound"];
-                    outbound["sat"] = res2["outbound"];
-                }else{message.push("No Timetable For Saturday");}
-                
-                if(sunResults !== null){
-                    var res3 = inboundOutbound(sunResults, firstDirection);
-                    inbound["sun"] = res3["inbound"];
-                    outbound["sun"] = res3["outbound"];
-                }else{message.push("No Timetable For Sunday/Bank Holiday");}
-                
-                
-                firstInStop = inbound["monFri"][0].name;
-                inboundStops.push(firstInStop);
-                //For loop to get the stop names
-                for(i = 0; i < inbound["monFri"].length; i++){
-                    if(inbound["monFri"][i].name !== inboundStops[count]){
-                        inboundStops.push(inbound["monFri"][i].name);
-                        count++;
+                    var firstInStop = null;
+                    var firstOutStop = null;
+                    var inboundStops = [];
+                    var outboundStops = [];
+                    var count = 0;
+                    var inNumCols = new Object();
+                    var outNumCols = new Object();
+                    var inbound = new Object();
+                    var outbound = new Object();
+                    var firstDirection = monFriResults[0].direction;
+
+                    if(monFriResults !== null){
+                        var res1 = inboundOutbound(monFriResults, firstDirection);
+                        inbound["monFri"] = res1["inbound"];
+                        outbound["monFri"] = res1["outbound"];
+                    }else{message.push("No Timetable For Midweek");}
+
+                    if(satResults !== null){
+                        var res2 = inboundOutbound(satResults, firstDirection);
+                        inbound["sat"] = res2["inbound"];
+                        outbound["sat"] = res2["outbound"];
+                    }else{message.push("No Timetable For Saturday");}
+
+                    if(sunResults !== null){
+                        var res3 = inboundOutbound(sunResults, firstDirection);
+                        inbound["sun"] = res3["inbound"];
+                        outbound["sun"] = res3["outbound"];
+                    }else{message.push("No Timetable For Sunday/Bank Holiday");}
+
+
+                    firstInStop = inbound["monFri"][0].name;
+                    inboundStops.push(firstInStop);
+                    //For loop to get the stop names
+                    for(i = 0; i < inbound["monFri"].length; i++){
+                        if(inbound["monFri"][i].name !== inboundStops[count]){
+                            inboundStops.push(inbound["monFri"][i].name);
+                            count++;
+                        }
+                        else if(inboundStops[count] === inbound["monFri"][i].name){
+
+                        }
                     }
-                    else if(inboundStops[count] === inbound["monFri"][i].name){
-                        
+
+                    count = 0;
+                    firstOutStop = outbound["monFri"][0].name;
+                    outboundStops.push(firstOutStop);
+                    //For loop to get the stop names
+                    for(i = 0; i < outbound["monFri"].length; i++){
+                        if(outbound["monFri"][i].name !== outboundStops[count]){
+                            outboundStops.push(outbound["monFri"][i].name);
+                            count++;
+                        }
+                        else if(outboundStops[count] === outbound["monFri"][i].name){
+
+                        }
                     }
+
+                    inNumCols["monFri"] = getCols(inbound["monFri"])/inboundStops.length;
+                    outNumCols["monFri"] = getCols(outbound["monFri"])/outboundStops.length;
+                    inNumCols["sat"] = getCols(inbound["sat"])/inboundStops.length;
+                    outNumCols["sat"] = getCols(outbound["sat"])/outboundStops.length;
+                    inNumCols["sun"] = getCols(inbound["sun"])/inboundStops.length;
+                    outNumCols["sun"] = getCols(outbound["sun"])/outboundStops.length;
+
+                    $.mobile.changePage('#displayTimetable');
+
+                    $('#header1').append("<b>"+inbound["monFri"][0].direction+"</b>");
+                    generateTables("Monday to Friday", '#inbound', inbound["monFri"], inboundStops, inNumCols["monFri"]);
+                    generateTables("Saturday", '#inbound', inbound["sat"], inboundStops, inNumCols["sat"]);
+                    generateTables("Sunday/Bank Holidays", '#inbound', inbound["sun"], inboundStops, inNumCols["sun"]);
+
+                    $('#header2').append("<b>"+outbound["monFri"][0].direction+"</b>");
+                    generateTables("Monday to Friday", '#outbound', outbound["monFri"], outboundStops, outNumCols["monFri"]);
+                    generateTables("Saturday", '#outbound', outbound["sat"], outboundStops, outNumCols["sat"]);
+                    generateTables("Sunday/Bank Holidays", '#outbound', outbound["sun"], outboundStops, outNumCols["sun"]);
+
+                } else if (data.error === true) {
+
                 }
-                
-                count = 0;
-                firstOutStop = outbound["monFri"][0].name;
-                outboundStops.push(firstOutStop);
-                //For loop to get the stop names
-                for(i = 0; i < outbound["monFri"].length; i++){
-                    if(outbound["monFri"][i].name !== outboundStops[count]){
-                        outboundStops.push(outbound["monFri"][i].name);
-                        count++;
-                    }
-                    else if(outboundStops[count] === outbound["monFri"][i].name){
-
-                    }
-                }
-                
-                inNumCols["monFri"] = getCols(inbound["monFri"])/inboundStops.length;
-                outNumCols["monFri"] = getCols(outbound["monFri"])/outboundStops.length;
-                inNumCols["sat"] = getCols(inbound["sat"])/inboundStops.length;
-                outNumCols["sat"] = getCols(outbound["sat"])/outboundStops.length;
-                inNumCols["sun"] = getCols(inbound["sun"])/inboundStops.length;
-                outNumCols["sun"] = getCols(outbound["sun"])/outboundStops.length;
-                
-                $.mobile.changePage('#displayTimetable');
-                
-                $('#header1').append("<b>"+inbound["monFri"][0].direction+"</b>");
-                generateTables("Monday to Friday", '#inbound', inbound["monFri"], inboundStops, inNumCols["monFri"]);
-                generateTables("Saturday", '#inbound', inbound["sat"], inboundStops, inNumCols["sat"]);
-                generateTables("Sunday/Bank Holidays", '#inbound', inbound["sun"], inboundStops, inNumCols["sun"]);
-                
-                $('#header2').append("<b>"+outbound["monFri"][0].direction+"</b>");
-                generateTables("Monday to Friday", '#outbound', outbound["monFri"], outboundStops, outNumCols["monFri"]);
-                generateTables("Saturday", '#outbound', outbound["sat"], outboundStops, outNumCols["sat"]);
-                generateTables("Sunday/Bank Holidays", '#outbound', outbound["sun"], outboundStops, outNumCols["sun"]);
-                
-            } else if (data.error === true) {
-
+            },
+            error: function (data) {
+                $('#tableSelectError').html("<center>Whoops! Something has gone wrong. Check Internet Connection and Try Again!</center>");
+                hideShowAlert('#tableSelectError');
+                console.log(data);
             }
-        },
-        error: function (data) {
-            $('#tableSelectError').html("<center>Whoops! Something has gone wrong. Check Internet Connection and Try Again!</center>");
-            hideShowAlert('#tableSelectError');
-            console.log(data);
-        }
-    });
-    
-    return false;
+        });
+
+        return false;
+    }
 });
+    
 
 function getCols(obj){
     var count = 0;
@@ -139,12 +145,12 @@ function generateTables(capt, divId, trips, stops, numCols){//, directionFlag){
     var colsHolder = numCols;
     
     for (i = 0; i < stops.length; i++) {
-        html += '<tr><td id="stops">' + stops[i] + '</td>';
-
+        html += '<tr><th id="stops">' + stops[i] + '</th>';
+        
         for (; count < colsHolder; count++) {
             html += '<td id="times"><b>' + trips[count].time.slice(0, 5) + '</b></td>';
         }
-        html += '</td>';
+        html += '</tr>';
         colsHolder = numCols*multiplier;
         multiplier++;
     }
--- js/map.js
+++ js/map.js
@@ -1,17 +1,67 @@
 var map;
+var polys = [];
+var dunDrogDub, bTown, ifsc, dunDrogDubStops, bTownStops, ifscStops;
+var routes = ["Dundalk-Drogheda-Dublin", "Bettystown-Laytown-Dublin", "IFSC"];
+var userIcon = L.MakiMarkers.icon({icon: "pitch", color: "#228b22", size: "m"});
+var busIcon = L.MakiMarkers.icon({icon: "bus", color: "#29ABE2", size: "m"});
+var stopIcon = L.MakiMarkers.icon({icon: "circle", color: "#FF0000", size: "m"});
+dunDrogDubStops = L.layerGroup();
+bTownStops = L.layerGroup();
+//dunDrogDubStops = L.layerGroup();
 
-map = L.map('map');
+getRoute(routes[0]);
+getRoute(routes[1]);
+getRoute(routes[2]);
+getStops(routes[0], 0);
+getStops(routes[1], 1);
+//getStops(routes[2]);
 
-L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+dunDrogDub = L.layerGroup([polys[0]]);
+bTown = L.layerGroup([polys[1]]);
+ifsc = L.layerGroup([polys[2]]);
+
+var OpenMapSurfer_Roads = L.tileLayer('http://korona.geog.uni-heidelberg.de/tiles/roads/x={x}&y={y}&z={z}', {
+    maxZoom: 18,
+    attribution: 'Imagery from <a href="http://giscience.uni-hd.de/">GIScience Research Group @ University of Heidelberg</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+});
+
+var osmDefault = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 18,
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+});
+
+map = L.map('map', {zoomControl: false, layers: [osmDefault, dunDrogDub, bTown, ifsc, dunDrogDubStops, bTownStops]});
+
+var baseMaps = {
+    "Default": osmDefault,
+    "Roads": OpenMapSurfer_Roads
+};
+
+var routeLayer = {
+    "Routes":{
+        "Dundalk-Drogheda-Dublin": dunDrogDub,
+            "Bettystown-Laytown-Dublin": bTown,
+                "IFSC": ifsc
+    },
+    "Stops":{
+        "Dundalk-Drogheda-Dublin": dunDrogDubStops,
+            "Bettystown-Laytown-Dublin": bTownStops
+    }
+};
+
+var options = {
+    exclusiveGroups: ["Routes"]
+};
+
+var layerControl = L.control.groupedLayers(baseMaps, routeLayer, {
+    collapsed: true
 }).addTo(map);
 
-var userIcon = L.icon({
-    iconUrl: '../images/userMapPin.png',
-    iconSize: [38, 85],
-    iconAnchor: [22, 94]
-});
+map.removeLayer(dunDrogDub);
+map.removeLayer(bTown);
+map.removeLayer(ifsc);
+map.removeLayer(dunDrogDubStops);
+map.removeLayer(bTownStops);
 
 map.locate({
     setView: true,
@@ -25,71 +75,76 @@ map.on('locationerror', onLocationError);
 function onLocationFound(e) {
     var radius = e.accuracy / 2;
 
-    L.marker(e.latlng, {
-        icon: userIcon
-    }).addTo(map)
-        .bindPopup("You are within " + radius + " meters of this point.").openPop$
+    L.marker(e.latlng,{icon: userIcon}).addTo(map)
+        .bindPopup(""+sessionStorage.user).openPop$
 }
 
 function onLocationError(e) {
     alert(e.message);
 }
 
-$.ajax({
-    type: "GET",
-    url: "https://www.geobus.co.uk/api/v1/getLocation",
-    success: function (data) {
-        if (data.error == false) {
-            var locations = data.data.locations;
-            var pointList = [];
-            for (i = 0; i < locations.length; i++) {
-                pointList[i] = new L.latLng(locations[i].Xpos, locations[i].Ypos);
+function createLayer(data){
+    layer.push(data);
+}
+
+function getStops(route, id){
+    $.ajax({
+        type: "GET",
+        url: "https://www.geobus.co.uk/api/v1/getStops/"+route,
+        async: false,
+        success: function (data) {
+            if (data.error == false) {
+                var stops = data.data.stops;
+                var marker;
+                var layerToAdd;
+                if(id === 0){layerToAdd = dunDrogDubStops;}
+                else if(id === 1){layerToAdd = bTownStops;}
+                for (i = 0; i < stops.length; i++) {    
+                    marker = new L.marker([stops[i].Xpos, stops[i].Ypos], {icon: stopIcon}).bindPopup(stops[i].name).addTo(layerToAdd);
+                }
+
+            } else if (data.error == true) {
+                //hideShowAlert($('#failAlert'));
             }
-
-            var polyline = L.polyline(pointList);
-            polyline.addTo(map).bindPopup("Route: Dublin->Drogheda->Dundalk");
-
-        } else if (data.error == true) {
-            //hideShowAlert($('#failAlert'));
+        },
+        error: function (data) {
+            console.log(data);
+            //console.log(result.error+"\n"+result.user+"\n"+result.token+"\n"+result.message);
         }
-    },
-    error: function (data) {
-        console.log(data);
-    }
-});
-
-/*
-$.ajax({
-    type: "GET",
-    url: "https://www.geobus.co.uk/api/v1/getStops",
-    success: function (data) {
-        if (data.error == false) {
-            var stops = data.data.stops;
-            var markerList = [];
-            for (i = 0; i < stops.length; i++) {
-                marker = new L.marker([stops[i].Xpos, stops[i].Ypos]).bindPopup(stops[i].name).addTo(map); //.bindPopup(stops.name[i]);
-            }
-
-        } else if (data.error == true) {
-            //hideShowAlert($('#failAlert'));
-        }
-    },
-    error: function (data) {
-        console.log(data);
-        //console.log(result.error+"\n"+result.user+"\n"+result.token+"\n"+result.message);
-    }
-});
-*/
-
-
-    $('#submitRoute').click(function(){
-        var route = $('#submitRoute').val();
-        console.log(route);
-    
-
-        return false;
     });
-    /*L.easyButton('<img src="../images/locateUser.png">', function(btn, map){
+}
 
-    }).addTo(map);*/
-//});*/
+function getRoute(route){
+    $.ajax({
+        type: "GET",
+        url: "https://www.geobus.co.uk/api/v1/getRouteMap/"+route,
+        async: false,
+        success: function (data) {
+            if (data.error == false) {
+                var locations = data.data.route;
+                var pointList = [];
+                for (i = 0; i < locations.length; i++) {
+                    pointList[i] = new L.latLng(locations[i].Xpos, locations[i].Ypos);
+                }
+
+                var polyline = L.polyline(pointList);
+                polyline.bindPopup("Route: "+route);
+                polys.push(polyline);
+
+            } else if (data.error == true) {
+                //hideShowAlert($('#failAlert'));
+            }
+        },
+        error: function (data) {
+            console.log(data);
+        }
+    });
+}
+
+function refresh(){
+    location.reload();
+}
+
+function center(){
+    map.locate({setView : true});
+}
